e55502be063af693a4acf64676edf5cddef969d0
Rithik M <rithik7899@gmail.com>
2025-06-02 10:20:52 +0530

    .

--- slices/slice.go
+++ slices/slice.go
@@ -1,12 +1,35 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Print(concat([]string{"A", "B"}, []string{"C", "D", "E"}))
+// }
+
+//	func concat(s1, s2 []string) []string {
+//		s1 = append(s1, s2...)
+//		return s1
+//	}
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	fmt.Print(concat([]string{"A", "B"}, []string{"C", "D", "E"}))
+type User struct {
+	Name string
 }
 
-func concat(s1, s2 []string) []string {
-	s1 = append(s1, s2...)
-	return s1
+func getUser(id int) (*User, error) {
+	// Simulating: user not found
+	return nil, fmt.Errorf("user with ID %d not found", id)
+}
+
+func main() {
+	_, err := getUser(42)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("User not found.")
+	}
 }
